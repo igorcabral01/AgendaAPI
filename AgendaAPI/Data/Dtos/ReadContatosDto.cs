900b84820a4bff990bef5fc86b9a7eb9f5d2e7de
Igorc <Igorc@DESKTOP-LLQ4I1J>
2024-08-10 09:32:46 -0300

    V.1

--- AgendaAPI/Data/Dtos/ReadContatosDto.cs
+++ AgendaAPI/Data/Dtos/ReadContatosDto.cs
@@ -4,7 +4,7 @@ namespace AgendaAPI.Data.Dtos
 {
     public class ReadContatosDto
     {
-
+        public int Id { get; set; }
         public  required string Nome { get; set; }
         public required string Email { get; set; }
         public int Telefone { get; set; }
